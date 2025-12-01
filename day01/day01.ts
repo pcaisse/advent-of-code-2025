37fc2f9fe6f756f12d5a111019c2f97b24e05bcc
Peter Caisse <pcaisse@gmail.com>
2025-12-01 09:47:10 -0500

    Day 1 solution 1

---
+++ day01/day01.ts
@@ -0,0 +1,36 @@
+function normalize(x: number) {
+  const n = x % 100;
+  return n < 0 ? n + 100 : n;
+}
+
+const input = await Bun.stdin.text();
+const start = 50;
+
+const directionOp = {
+  L: (x: number, y: number) => x - y,
+  R: (x: number, y: number) => x + y,
+};
+
+const ret = input
+  .split("\n")
+  .slice(0, -1)
+  .map(
+    ([direction, ...x]) =>
+      ({ direction, value: Number(x.join("")) }) as {
+        direction: "L" | "R";
+        value: number;
+      },
+  )
+  .reduce(
+    ({ pos, count }, { direction, value }) => {
+      const op = directionOp[direction];
+      const newPos = normalize(op(pos, value));
+      return {
+        pos: newPos,
+        count: newPos === 0 ? count + 1 : count,
+      };
+    },
+    { pos: start, count: 0 },
+  );
+
+console.log(ret);
