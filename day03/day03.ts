f6e40771051366656ff5c0c7efb02337ea51dcd5
Peter Caisse <pcaisse@gmail.com>
2025-12-03 15:43:04 -0500

    Day 3 solution 2 WIP

--- day03/day03.ts
+++ day03/day03.ts
@@ -1,16 +1,82 @@
 const input = await Bun.stdin.text();
 
-function findLargestJoltage(
-  batteries: { value: number; index: number }[],
-): number {
-  const batteriesDesc = batteries.sort((a, b) => b.value - a.value);
-  const first = batteriesDesc.find(
-    ({ index }) => index !== batteriesDesc.length - 1,
-  );
-  if (!first) throw Error("first missing");
-  const second = batteriesDesc.find(({ index }) => index > first.index);
-  if (!second) throw Error("second missing");
-  return Number(String(first.value) + String(second.value));
+interface Battery {
+  value: number;
+  index: number;
+  skip: boolean;
+}
+
+const numDigits = 12;
+
+const debug = false;
+
+function formatBatteries(batteries: Battery[]) {
+  return batteries.map(({ value }) => String(value)).join("");
+}
+
+function processBatteries(batteries: Battery[]): Battery[] {
+  let remainingSkips = batteries.length - numDigits;
+
+  const skipBattery = (
+    battery: Battery,
+    label: string,
+    currBattery: Battery,
+  ) => {
+    if (debug) {
+      console.log(
+        "skipping",
+        label,
+        battery,
+        `because its value of ${battery.value} is less than or equal to ${currBattery.value}`,
+        currBattery,
+      );
+    }
+    battery.skip = true;
+    remainingSkips--;
+  };
+
+  for (let i = 0; i < batteries.length; i++) {
+    const currBattery = batteries[i];
+    const prevBattery = batteries[i - 1];
+    if (
+      remainingSkips > 0 &&
+      currBattery &&
+      prevBattery &&
+      !prevBattery.skip &&
+      currBattery.value >= prevBattery.value
+    ) {
+      skipBattery(prevBattery, "regular", currBattery);
+      // TODO: Figure out issue with "backwards skipping" where fixing up skips that result from
+      // non-immediate repeated number (sequences of numbers that become repeated after other
+      // numbers are skipped) are not handled correctly.
+      for (let j = i - 1; j >= 0; j--) {
+        const pastBattery = batteries[j];
+        if (
+          remainingSkips > 0 &&
+          pastBattery &&
+          !pastBattery.skip &&
+          currBattery.value > pastBattery.value
+        ) {
+          skipBattery(pastBattery, "past", currBattery);
+        }
+      }
+    }
+  }
+  return batteries.filter(({ skip }) => !skip);
+}
+
+function findLargestJoltage(batteries: Battery[]): number {
+  const ret = processBatteries(batteries);
+
+  const unskippedBatteries = batteries.filter(({ skip }) => !skip);
+  if (unskippedBatteries.length !== numDigits) {
+    console.log("unskippedBatteries", formatBatteries(unskippedBatteries));
+    throw new Error(
+      `unskippedBatteries has incorrect length of ${unskippedBatteries.length}`,
+    );
+  }
+
+  return Number(formatBatteries(ret));
 }
 
 const ret = input
@@ -18,7 +84,11 @@ const ret = input
   .slice(0, -1)
   .map((bank) =>
     findLargestJoltage(
-      [...bank].map((value, index) => ({ value: Number(value), index })),
+      [...bank].map((value, index) => ({
+        value: Number(value),
+        index,
+        skip: false,
+      })),
     ),
   )
   .reduce((a, b) => a + b, 0);
