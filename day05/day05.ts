5e17f78a6fe4848178f9e618150b12c30e958e45
Peter Caisse <pcaisse@gmail.com>
2025-12-06 19:59:03 -0500

    Day 5 solution 2

--- day05/day05.ts
+++ day05/day05.ts
@@ -1,17 +1,52 @@
 const input = await Bun.stdin.text();
 
-const [ranges, ingredients] = input.split("\n\n");
+const [rangeInput, _] = input.split("\n\n");
 
-const rangeFuncs = (ranges as string).split("\n").map((s) => {
-  const [min, max] = s.split("-").map(Number) as [number, number];
-  return (x: number) => x >= min && x <= max;
-});
+function countRange([min, max]: [number, number]): number {
+  return max - min + 1;
+}
 
-const singleRangeFunc = (x: number) =>
-  rangeFuncs.reduce((acc, currFunc) => acc || currFunc(x), false);
+const ranges: [number, number][] = (rangeInput as string)
+  .split("\n")
+  .map((s) => {
+    return s.split("-").map(Number) as [number, number];
+  })
+  .sort(([minA, maxA], [minB, maxB]) => {
+    const minDiff = minA - minB;
+    if (minDiff === 0) {
+      return maxA - maxB;
+    }
+    return minDiff;
+  });
 
-const ingredientNumbers = (ingredients as string).split("\n").map(Number);
+let total = 0;
 
-const ret = ingredientNumbers.filter(singleRangeFunc).length;
+for (let i = 0; i < ranges.length; i++) {
+  const range = ranges[i];
+  if (range === undefined) {
+    throw new Error("wat");
+  }
+  const [min, max] = range;
+  const prev = ranges[i - 1];
+  if (prev && min >= prev[0] && max <= prev[1]) {
+    // Full overlap
+    ranges[i] = prev;
+    continue;
+  } else if (prev && max <= prev[1]) {
+    // Underlap
+    ranges[i] = prev;
+    continue;
+  } else if (prev && min <= prev[1]) {
+    // Partial overlap
+    const newRange: [number, number] = [prev[1] + 1, max];
+    ranges[i] = newRange;
+    const added = countRange(newRange);
+    total += added;
+  } else {
+    // Unchanged min/max
+    const added = countRange([min, max]);
+    total += added;
+  }
+}
 
-console.log(ret);
+console.log(total);
