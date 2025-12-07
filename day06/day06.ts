2ceddcef7ef04235f0e787095c22914f3201c16a
Peter Caisse <pcaisse@gmail.com>
2025-12-07 09:50:51 -0500

    Day 6 solution 2

--- day06/day06.ts
+++ day06/day06.ts
@@ -8,7 +8,7 @@ const opFunc = {
 function rotate(data: (number | string)[][]): (number | string)[][] {
   let ret: number[][] = [];
   for (let i = 0; i < data.length; i++) {
-    const row = data[i] as number[];
+    const row = data[i] as (number | string)[];
     for (let j = 0; j < row.length; j++) {
       if (!ret[j]) {
         ret[j] = [];
@@ -20,16 +20,42 @@ function rotate(data: (number | string)[][]): (number | string)[][] {
   return ret;
 }
 
-const ret = rotate(
-  input
-    .split("\n")
-    .slice(0, -1)
-    .map((s) => s.split(/\s/).filter((s) => s)),
-)
+const rawData = input.split("\n").slice(0, -1);
+
+const data = rawData.map((s) => s.split(""));
+
+// Figure out column widths since they are variable
+const splitData = rawData.map((s) => s.match(/\S+/g) || []);
+const rotatedSplitData = rotate(splitData);
+const isSeparatorColumnLookup: boolean[] = (rotatedSplitData as string[][])
+  .map(
+    (cols) =>
+      cols.reduce(
+        (largest, current) =>
+          current.length > largest.length ? current : largest,
+        "",
+      ).length,
+  )
+  // This is a bit wasteful since we only need to store the separator columns
+  .flatMap((value) => [...new Array(value)].map((_) => false).concat([true]));
+
+const processedData = data.map((row) =>
+  row
+    .map((value, index) => (isSeparatorColumnLookup[index] ? "|" : value))
+    .join("")
+    .split("|"),
+);
+
+const rotatedProcessedData = rotate(processedData);
+
+const ret = rotatedProcessedData
   .map((row) => {
-    const op = row.pop() as "*" | "+";
+    const op = (row.pop() as string).trim() as "*" | "+";
     const func = opFunc[op];
-    const nums = row.map(Number);
+    const rowArray = (row as string[]).map((s) => [...s]);
+    const rotatedRowArray = rotate(rowArray);
+    const rotatedRow = rotatedRowArray.map((row) => row.join(""));
+    const nums = rotatedRow.map(Number);
     return nums.reduce((acc, value) => func(acc, value), op === "*" ? 1 : 0);
   })
   .reduce((x, y) => x + y, 0);
